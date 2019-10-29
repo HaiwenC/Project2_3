5b9141b0ec404b0cf5268a1d937f8aee6d88cb14
Mingchen Zhang <zhan307@usc.edu>
2019-10-29 10:28:24 -0700

    calendars.java

--- app/src/main/java/model/Calendars.java
+++ app/src/main/java/model/Calendars.java
@@ -11,7 +11,7 @@ import java.util.*;
  * @author      Mingchen Zhang
  */
 public class Calendars {
-    Set<Calendar> myCalendars;
+    private Set<Calendar> myCalendars;
     /**
      * get myCalendars
      */
