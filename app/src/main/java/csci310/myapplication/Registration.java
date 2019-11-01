9257dc1c6f9de9233f591975e87ed3c4ab3f5094
Beiyou ZHU <zhubeiyou2@gmail.com>
2019-10-31 21:41:07 -0700

    modify registration

--- app/src/main/java/csci310/myapplication/Registration.java
+++ app/src/main/java/csci310/myapplication/Registration.java
@@ -98,35 +98,3 @@ public class Registration extends AppCompatActivity {
                 });
     }
 }
-/*                        .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                Log.d("TAG", "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                if (!task.isSuccessful()) {
-                                    // Show the message task.getException()
-                                } else {
-                                    // successfully account created
-                                    // now the AuthStateListener runs the onAuthStateChanged callback
-
-                                    FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
-                                        @Override
-                                        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                                            FirebaseUser user = firebaseAuth.getCurrentUser();
-                                            if (user != null) {
-                                                // User is signed in
-                                                // NOTE: this Activity should get onpen only when the user is not signed in, otherwise
-                                                // the user will receive another verification email.
-                                                sendVerificationEmail();
-                                            } else {
-                                                // User is signed out
-
-                                            }
-                                        }
-                                    };
-                                }
-                            }
-                        });*/
