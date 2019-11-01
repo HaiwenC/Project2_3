bb54764e47d56a5aeb77b393c6ea74d00fffe504
haiwenc@usc.edu <haiwenc@usc.edu>
2019-10-31 23:29:04 -0700

    u

--- app/src/main/java/model/Request.java
+++ app/src/main/java/model/Request.java
@@ -4,17 +4,22 @@ public class Request {
     private Tutee tutee;
     private Tutor tutor;
     private String subject;
+    private int dayOfWeek;
     private int timeBegin;
     private int index;
     private String status;
 
-    public Request(Tutee tutee, Tutor tutor, String className, int begin, int ind){
+    public Request(Tutee tutee, Tutor tutor, String className, int day ,int begin, int ind){
         this.tutee = tutee;
         this.tutor = tutor;
         this.subject = className;
+        this.dayOfWeek = day;
         this.timeBegin = begin;
         this.index = ind;
+
+        //possible status: 1:"accepted" 2:"rejected" 3:"available"
         this.status = "available";
+
     }
 
     public Request(Tutee tutee, Tutor t, String s, Integer integer, Integer integer1, int i) {
@@ -25,10 +30,22 @@ public class Request {
 //        Session newSession = new Sections(timeBegin, timeEnd);
         Session newSession;
         tutee.addSession(newSession);
-        tutor.addSections(newSession);
+        tutor.addSections(newSession, dayOfWeek, timeBegin);
+        status = "accepted";
         //TODO add session to database
     }
 
+    public void rejectRequest(){
+        status = "rejected";
+    }
+
+    public int getDayOfWeek(){
+        return dayOfWeek;
+    }
+
+    public int getTime(){
+        return timeBegin;
+    }
 
     public Tutor getTutor() {
         return tutor;
