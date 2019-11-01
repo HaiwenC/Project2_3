1667c29ff543b9e7bcc4c3aa0329b85c1d0af159
Beiyou ZHU <zhubeiyou2@gmail.com>
2019-10-31 21:23:31 -0700

    email verification

--- app/src/main/java/csci310/myapplication/Registration.java
+++ app/src/main/java/csci310/myapplication/Registration.java
@@ -1,29 +1,132 @@
 package csci310.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Registration extends AppCompatActivity {
     private Button SaveButton;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private EditText email;
+    private EditText passWord;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
         SaveButton = findViewById(R.id.saveButton);
+        email = findViewById(R.id.CreateEmail);
+        passWord = findViewById(R.id.CreatePassword);
+        //mAuth = FirebaseAuth.getInstance();
         SaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), passWord.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+
+                        }
+                        else{
+                            FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+                                @Override
+                                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                    if (user != null) {
+                                        // User is signed in
+                                        // NOTE: this Activity should get onpen only when the user is not signed in, otherwise
+                                        // the user will receive another verification email.
+                                        sendVerificationEmail();
+                                    } else {
+                                        // User is signed out
+
+                                    }
+                                }
+                            };
+                        }
+                    }
+                });
                 Toast.makeText(getApplicationContext(), "account created", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
     }
+    public void sendVerificationEmail()
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // email sent
+                            // after email is sent just logout the user and finish this activity
+                            FirebaseAuth.getInstance().signOut();
+                            Toast.makeText(getApplicationContext(), "success email verificaiton", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "email sent unsecessful verificaiton", Toast.LENGTH_SHORT).show();;
+                            // email not sent, so display message and restart the activity or do whatever you wish to do
+
+                            //restart this activity
+                            overridePendingTransition(0, 0);
+                            finish();
+                            overridePendingTransition(0, 0);
+                            startActivity(getIntent());
+
+                        }
+                    }
+                });
+    }
 }
+/*                        .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d("TAG", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    // Show the message task.getException()
+                                } else {
+                                    // successfully account created
+                                    // now the AuthStateListener runs the onAuthStateChanged callback
+
+                                    FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+                                        @Override
+                                        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                                            if (user != null) {
+                                                // User is signed in
+                                                // NOTE: this Activity should get onpen only when the user is not signed in, otherwise
+                                                // the user will receive another verification email.
+                                                sendVerificationEmail();
+                                            } else {
+                                                // User is signed out
+
+                                            }
+                                        }
+                                    };
+                                }
+                            }
+                        });*/
