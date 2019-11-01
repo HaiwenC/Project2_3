2f257411f2881c5c243816eb35e300a0f37f4f74
Beiyou ZHU <zhubeiyou2@gmail.com>
2019-11-01 14:47:42 -0700

    updates

--- app/src/main/java/csci310/myapplication/SearchPage.java
+++ app/src/main/java/csci310/myapplication/SearchPage.java
@@ -1,15 +1,14 @@
 package csci310.myapplication;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TuteeProfile extends AppCompatActivity {
+public class SearchPage extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tuteeprofilepage);
+        setContentView(R.layout.searchpage);
     }
 }
--- app/src/main/java/csci310/myapplication/TuteeHome.java
+++ app/src/main/java/csci310/myapplication/TuteeHome.java
@@ -1,15 +1,36 @@
 package csci310.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TuteeHome extends AppCompatActivity {
+    private Button profile;
+    private Button Search;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tuteepage);
+        profile = findViewById(R.id.ProfileButton);
+        Search = findViewById(R.id.Search);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), TuteeProfile.class);
+                startActivity(i);
+            }
+        });
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SearchPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
