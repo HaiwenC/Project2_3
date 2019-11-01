9b94f5000ba86a2635863676b81476374e336bcd
Beiyou ZHU <zhubeiyou2@gmail.com>
2019-11-01 14:38:04 -0700

    listview finished

--- app/src/main/java/csci310/myapplication/TutorHome.java
+++ app/src/main/java/csci310/myapplication/TutorHome.java
@@ -9,11 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,17 +27,17 @@ import model.Tutor;
 
 public class TutorHome extends AppCompatActivity {
     private Button profile;
-    private RecyclerView list;
-    //private MyRecyclerViewAdapter adapter;
+    private ListView list;
+    private RequestAdapter adapter;
     public static List<Request> groups = new ArrayList<Request>();
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        list = findViewById(R.id.listNotes);
-        //Log.d("debug listview", list.toString());
-        groups.add(new Request(new Tutee("1", "1", "1","1"),
-                new Tutor("1", "1", "!", "1"), "csci109", 1 ,8, 9));
         setContentView(R.layout.tutorpage);
+        list = findViewById(R.id.listNotes);
+        Log.d("debug listview", list.toString());
+        groups.add(new Request(new Tutee("1", "1", "Beiyou","1"),
+                new Tutor("1", "1", "!", "1"), "csci109", 1 ,8, 9));
         profile = findViewById(R.id.profile);
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,49 +46,30 @@ public class TutorHome extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        //adapter = new MyRecyclerViewAdapter(getApplicationContext(), groups);
+        adapter = new RequestAdapter(getApplicationContext(), 0, groups);
         //Log.d("debug adapter", adapter.toString());
-        //list.setAdapter(adapter);
+        list.setAdapter(adapter);
     }
-    /*private class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>{
-        private List<Request> mData;
-        private LayoutInflater mInflater;
-
-        // data is passed into the constructor
-        MyRecyclerViewAdapter(Context context, List<Request> data) {
-            this.mInflater = LayoutInflater.from(context);
-            this.mData = data;
+    private class RequestAdapter extends ArrayAdapter<Request> {
+        List<Request> Groups;
+        public RequestAdapter(Context context, int resource, List<Request> objects) {
+            super(context, resource, objects);
+            this.Groups = objects;
         }
-        // inflates the row layout from xml when needed
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = mInflater.inflate(R.layout.requestlayout, parent, false);
-            return new ViewHolder(view);
-        }
-        // binds the data to the TextView in each row
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            String animal = mData.get(position);
-            holder.myTextView.setText(animal);
-        }
-        // total number of rows
-        @Override
-        public int getItemCount() {
-            return mData.size();
-        }
-        // stores and recycles views as they are scrolled off screen
-        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-            TextView myTextView;
-
-            ViewHolder(View itemView) {
-                super(itemView);
-                myTextView = itemView.findViewById(R.id.tvAnimalName);
-                itemView.setOnClickListener(this);
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(
+                        R.layout.requestlayout, null);
             }
-            @Override
-            public void onClick(View view) {
-                if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-            }
+            TextView name = convertView.findViewById(R.id.RequestName);
+            TextView course = convertView.findViewById(R.id.RequestCourse);
+            TextView period = convertView.findViewById(R.id.TimePeriod);
+            Request gp = Groups.get(position);
+            name.setText(gp.getTutee().getName());
+            course.setText(gp.getSubject());
+            period.setText(String.valueOf(gp.getTime()));
+            return convertView;
         }
-    }*/
+    }
 }
