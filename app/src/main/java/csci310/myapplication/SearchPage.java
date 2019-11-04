ba95632ee0b62815cbcec83a9532308085c9afea
Beiyou ZHU <zhubeiyou2@gmail.com>
2019-11-04 10:30:39 -0800

    no message

--- app/src/main/java/csci310/myapplication/SearchPage.java
+++ app/src/main/java/csci310/myapplication/SearchPage.java
@@ -1,19 +1,30 @@
 package csci310.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import model.Request;
 
 public class SearchPage extends AppCompatActivity {
     private Button save;
     private Button search;
+    private ListView list;
+    private SearchAdapter adapter;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchpage);
+        list = findViewById(R.id.listOfSearch);
         save = findViewById(R.id.SaveBut);
         search = findViewById(R.id.SearchBut);
         save.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +33,27 @@ public class SearchPage extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private class SearchAdapter extends ArrayAdapter<Request> {
+        List<Request> Groups;
+        public SearchAdapter(Context context, int resource, List<Request> objects) {
+            super(context, resource, objects);
+            this.Groups = objects;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(
+                        R.layout.requestlayout, null);
+            }
+            TextView name = convertView.findViewById(R.id.RequestName);
+            TextView course = convertView.findViewById(R.id.RequestCourse);
+            TextView period = convertView.findViewById(R.id.TimePeriod);
+            Request gp = Groups.get(position);
+            name.setText(gp.getTutee().getName());
+            course.setText(gp.getSubject());
+            period.setText(String.valueOf(gp.getTime()));
+            return convertView;
+        }
     }
 }
