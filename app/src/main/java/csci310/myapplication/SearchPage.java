aaed7f37b903aeb81f0ef93d43dacc8aee2d27ad
Jiawei Wang <513203628@qq.com>
2019-11-04 21:39:44 -0800

    Javen

--- app/src/main/java/csci310/myapplication/SearchPage.java
+++ app/src/main/java/csci310/myapplication/SearchPage.java
@@ -3,26 +3,46 @@ package csci310.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import model.Request;
+import model.Tutee;
 import model.Tutor;
+
+import static csci310.myapplication.MainActivity.tuteeRefe;
+import static csci310.myapplication.MainActivity.tutorRefe;
 
 public class SearchPage extends AppCompatActivity {
     private Button save;
     private Button search;
     private ListView list;
     private SearchAdapter adapter;
+    private String subject;
+    //    private EditText subject;
     private List<Tutor> Requests = new ArrayList<>();
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +51,8 @@ public class SearchPage extends AppCompatActivity {
         list = findViewById(R.id.listOfSearch);
         save = findViewById(R.id.SaveBut);
         search = findViewById(R.id.SearchBut);
+        subject = "Math";
+//        subject = findViewById(R.id.Subject);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,7 +64,41 @@ public class SearchPage extends AppCompatActivity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Query query = tutorRefe.whereEqualTo("subject", subject);
+                query.get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                boolean isExist = false;
+                                String password = "";
+                                String studentID = "";
+                                String name      = "";
+                                String username  = "";
+                                String email     = "";
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d("qqqq", document.getId() + " => " + document.getData());
+                                        isExist = true;
+                                        password = document.getData().get("password").toString();
+                                        studentID = document.getData().get("studentID").toString();
+                                        name      = document.getData().get("name").toString();
+                                        username  = document.getData().get("username").toString();
+                                        email  = document.getData().get("email").toString();
+                                        Tutor tutorNew = new Tutor(studentID, email ,name, username, password);
+                                        Requests.add(tutorNew);
+                                    }
+                                    if (isExist) {
+                                        Toast.makeText(getApplicationContext(), "This user is existed", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        CollectionReference citiesRefInside;
+                                        Toast.makeText(getApplicationContext(), "User added", Toast.LENGTH_LONG).show();
+                                    }
 
+                                } else {
+                                    Log.d("qqqq","Error, can't run query");
+                                }
+                            }
+                        });
             }
         });
     }
