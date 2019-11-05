3f5bd8475bfb284fe41ccfeb7da85027dd4e8bdd
Beiyou ZHU <zhubeiyou2@gmail.com>
2019-11-04 21:08:19 -0800

    update search

--- app/src/main/java/csci310/myapplication/SearchPage.java
+++ app/src/main/java/csci310/myapplication/SearchPage.java
@@ -1,6 +1,7 @@
 package csci310.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.Nullable;
@@ -20,6 +22,7 @@ public class SearchPage extends AppCompatActivity {
     private Button search;
     private ListView list;
     private SearchAdapter adapter;
+    private List<Request> Requests = new ArrayList<>();
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +34,14 @@ public class SearchPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+        adapter = new SearchAdapter(getApplicationContext(), 0, Requests);
+        list.setAdapter(adapter);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
