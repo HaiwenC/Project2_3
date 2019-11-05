86ea7671891855a3102b42c0d55d9f734d712506
Beiyou ZHU <zhubeiyou2@gmail.com>
2019-11-04 19:42:53 -0800

    no message

--- app/src/main/java/csci310/myapplication/TutorProfile.java
+++ app/src/main/java/csci310/myapplication/TutorProfile.java
@@ -4,16 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TutorProfile extends AppCompatActivity {
     private Button update;
+    private TextView name;
+    private TextView courseName;
+    private TextView Dayofweek;
+    private TextView email;
+    private TextView period;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profilepage);
+        name = findViewById(R.id.TutorName);
+        courseName = findViewById(R.id.CourseNameId);
+        email = findViewById(R.id.ProfileEmail);
+        Dayofweek = findViewById(R.id.ProfileDayOfWeek);
+        period = findViewById(R.id.ProfileTimePeriod);
         update = findViewById(R.id.SaveBut);
         update.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +33,8 @@ public class TutorProfile extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        name.setText(MainActivity.tutorInfo.getName());
+        courseName.setText(MainActivity.tutorInfo.getSubject().get(0));
+        period.setText(MainActivity.tutorInfo.getTime());
     }
 }
