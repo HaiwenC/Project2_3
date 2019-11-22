3a4f8629f9210fae8afae0f2c2f3be1148c09a59
Jiawei Wang <513203628@qq.com>
2019-11-21 17:24:08 -0800

    .

---
+++ app/src/androidTest/java/csci310/myapplication/DatabaseTest.java
@@ -0,0 +1,13 @@
+package csci310.myapplication;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DatabaseTest {
+
+}
